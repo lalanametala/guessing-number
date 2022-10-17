66f0aa2b51b0c0c87d79a3c89576ace619aada5d
Lalá Nametala <lalanametala@gmail.com>
2022-10-17 15:49:44 -0300

    :test_tube: test(TestFirstReq): implements the Greet and ChooseNumber tests

--- src/guessing-number.Test/TestFirstReq.cs
+++ src/guessing-number.Test/TestFirstReq.cs
@@ -10,30 +10,85 @@ namespace guessing_number.Test;
 public class TestFirstReq
 {
     [Theory(DisplayName = "Deve exibir mensagem inicial!")]
-    [InlineData(new object[] {new string[]{"---Bem-vindo ao ...---", "Para começar..."}})]
+    [InlineData(new object[] {new string[]{"---Bem-vindo ao Guessing Game---", "Para começar, tente adivinhar o número que eu pensei, entre -100 e 100!"}})]
     public void TestPrintInitialMessage(string[] expected)
     {
-        throw new NotImplementedException();                                                   
+        GuessNumber instance = new();
+        string[] consoleResponse;
+        using(var stringWriter = new StringWriter()) 
+        {
+            Console.SetOut(stringWriter);
+            instance.Greet();
+            consoleResponse = stringWriter.ToString().Trim().Split('\n');
+        }
+        consoleResponse.Should().BeEquivalentTo(expected);                                              
     }
 
     [Theory(DisplayName = "Deve receber a entrada do usuário e converter para int")]
     [InlineData("0", 0)]
     public void TestReceiveUserInputAndConvert(string entry, int expected)
     {        
-        throw new NotImplementedException();
+        GuessNumber instance = new();
+        using(var stringWriter = new StringWriter())
+        {
+            using(var stringReader = new StringReader(entry))
+            {
+                Console.SetOut(stringWriter);
+                Console.SetIn(stringReader);
+
+                instance.ChooseNumber();                
+            }
+            instance.userValue.Should().Be(expected);
+        };
     }
 
     [Theory(DisplayName = "Deve receber a entrada do usuário e garantir que é um numérico")]
     [InlineData(new object[] {new string[]{"10,", "10"}, 10})]
     public void TestReceiveUserInputAndVerifyType(string[] entrys, int expected)
     {
-        throw new NotImplementedException();  
+        string stringReaderEntry = "";
+
+        foreach (var entry in entrys)
+        {
+            stringReaderEntry += entry + "\n";
+        }
+
+        GuessNumber instance = new();
+        using(var stringWriter = new StringWriter())
+        {
+            using(var stringReader = new StringReader(stringReaderEntry))
+            {
+                Console.SetOut(stringWriter);
+                Console.SetIn(stringReader);
+
+                instance.ChooseNumber();                
+            }
+            instance.userValue.Should().Be(expected);
+        };
     }
 
     [Theory(DisplayName = "Deve receber a entrada do usuário e garantir que está entre -100 e 100!")]
     [InlineData(new object[] {new string[]{"1000", "10"}, 10})]
     public void TestReceiveUserInputAndVerifyRange(string[] entrys, int expected)
     {
-        throw new NotImplementedException();
+        string stringReaderEntry = "";
+
+        foreach (var entry in entrys)
+        {
+            stringReaderEntry += entry + "\n";
+        }
+
+        GuessNumber instance = new();
+        using(var stringWriter = new StringWriter())
+        {
+            using(var stringReader = new StringReader(stringReaderEntry))
+            {
+                Console.SetOut(stringWriter);
+                Console.SetIn(stringReader);
+
+                instance.ChooseNumber();                
+            }
+            instance.userValue.Should().Be(expected);
+        };
     }    
 }
