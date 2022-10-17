0280318fca69e20e4b0e9c5f6119be8f728b845a
Lalá Nametala <lalanametala@gmail.com>
2022-10-17 20:11:45 -0300

    :test_tube: test(TestThirdReq): implements the tests for all methods

--- src/guessing-number.Test/TestThirdReq.cs
+++ src/guessing-number.Test/TestThirdReq.cs
@@ -14,6 +14,37 @@ public class TestThirdReq
     [InlineData(new object[] {new string[]{"10"}, 10})]
     public void TestFullFlow(string[] entrys, int mockValue)
     {
-        throw new NotImplementedException();
+        string stringReaderEntry = "";
+
+        foreach (var entry in entrys)
+        {
+            stringReaderEntry += entry + "\n";
+        }
+
+        Mock<IRandomGenerator> getInt = new();
+        getInt.Setup(instance => instance.GetInt(It.IsAny<int>(), It.IsAny<int>())).Returns(mockValue);
+        GuessNumber instance = new(getInt.Object);
+
+        string[] consoleResponse;
+
+        using(var stringWriter = new StringWriter())
+        {
+            using(var stringReader = new StringReader(stringReaderEntry))
+            {
+                Console.SetOut(stringWriter);
+                Console.SetIn(stringReader);
+                instance.Greet();
+                instance.RandomNumber();
+                
+                do
+                {
+                    instance.ChooseNumber();
+                    instance.AnalyzePlay();
+                } while (instance.userValue != instance.randomValue);
+                consoleResponse = stringWriter.ToString().Trim().Split("\n");
+            }
+            instance.userValue.Should().Be(mockValue);
+            consoleResponse[^1].Should().Contain("ACERTOU!");
+        };
     }
 }
